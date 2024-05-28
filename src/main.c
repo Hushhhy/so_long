f76c7188d90606cf60c6826791ea7c6b410eb979
CptrAlexis <99977761+SaturnSpace@users.noreply.github.com>
2024-05-28 16:00:02 +0200

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: acarpent <acarpent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 14:34:09 by acarpent          #+#    #+#             */
-/*   Updated: 2024/05/27 14:03:27 by acarpent         ###   ########.fr       */
+/*   Updated: 2024/05/28 12:58:44 by acarpent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ int	main(int ac, char **av)
 {
 	if (ac == 2)
     {
-        
+        ft_initmap(av[1]);
     }
     else
     {
-        ft_printf("Argument number must be 1 !");
+        ft_printf("Error\nArgument number must be 1 !");
         return (0);
     }
-    retun (0);
+    return (0);
 }
--- src/parsing.c
+++ src/parsing.c
@@ -1,36 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acarpent <acarpent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/16 14:24:26 by acarpent          #+#    #+#             */
-/*   Updated: 2024/05/28 12:59:04 by acarpent         ###   ########.fr       */
+/*   Created: 2024/05/28 10:56:30 by acarpent          #+#    #+#             */
+/*   Updated: 2024/05/28 12:58:10 by acarpent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
+#include "../include/so_long.h"
+#include <stdio.h>
 
-# include "../ft_printf/ft_printf.h"
-# include "../libft/libft.h"
+void	ft_initmap(char *map)
+{
+	char	*line;
+	int		fd;
 
-# include <stdlib.h>
-# include <unistd.h>
-# include <string.h>
-# include <X11/keysym.h>
-# include <X11/X.h>
-# include <fcntl.h>
-
-typedef struct t_map {
-	char	**map;
-	int		C;
-	int		E;
-	int		P;
-}				s_map;
-
-char	*get_next_line(int fd);
-void	ft_initmap(char *map);
-
-#endif
+	fd = open("map1.ber", O_WRONLY);
+	line = get_next_line(fd);
+	printf("%s\n", line);
+}
